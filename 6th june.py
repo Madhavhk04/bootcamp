0428f52e30315ec42425eb53f5ffbefc009a6cfa
Madhavhk04 <devuappu@gmail.com>
2024-06-06 12:15:18 +0400

    changes

--- 6th june.py
+++ 6th june.py
@@ -3,3 +3,13 @@ a=10
 b=20
 c=(a+b)
 print("c")
+
+a = 10
+b = 5
+
+if a > b:
+    print(f"{a} is greater than {b}")
+elif a < b:
+    print(f"{a} is less than {b}")
+else:
+    print(f"{a} is equal to {b}")
