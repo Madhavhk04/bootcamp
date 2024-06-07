4d3759896ca3605ec63ab01265f71eaf0147965a
Madhavhk04 <devuappu@gmail.com>
2024-06-07 09:53:12 +0400

    changess

--- 6th june.py
+++ 6th june.py
@@ -1,8 +1,8 @@
 print("hello world")
 a=10
 b=20
-c=(a+b)
-print("c")
+c=a+b
+print=c
 
 a = 10
 b = 5
@@ -13,3 +13,32 @@ elif a < b:
     print(f"{a} is less than {b}")
 else:
     print(f"{a} is equal to {b}")
+
+    # Jumping Statements
+# break statement
+for i in range(15):
+    if i == 8:
+        break
+    print(i)
+
+# continue statement
+for i in range(15):
+    if i == 8:
+        continue
+    print(i)
+
+def welcome(user): # Function definition with parameter 'user'
+    
+    print("Welcome", user + "!")
+
+# Function with argument 'aadhil'
+welcome("aadhil")
+
+#posistional arguments
+
+def compute_area(height, breadth):
+    return height * breadth
+
+
+area = compute_area(7, 4)  
+print(area)  
