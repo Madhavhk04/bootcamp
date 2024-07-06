a1eb2934e2707a51526e91588a101c5964dee573
Madhavhk04 <148519558+Madhavhk04@users.noreply.github.com>
2024-07-06 16:54:51 +0530

    cc

---
+++ 5thjuly.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# Load the data
+file_path = r"C:\Users\devua\OneDrive\Desktop\MADHAV\delhivery_data.csv"
+df = pd.read_csv(file_path)
+
+# Display the first few rows of the DataFrame
+df.head()
+
